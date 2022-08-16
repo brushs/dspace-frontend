b2246afa87c40a29f1f8b6f0c0c797ee31610857
Pragash <pragasra@gmail.com>
2022-08-16 08:03:35 -0400

    fixing theme to ospr in dev yaml

---
+++ src/themes/ospr/app/submission/form/submission-form.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import { SubmissionFormComponent as BaseComponent } from '../../../../../app/submission/form/submission-form.component';
+
+/**
+ * This component represents the submission form.
+ */
+@Component({
+  selector: 'ds-submission-form',
+  styleUrls: ['./submission-form.component.scss'],
+  //styleUrls: ['../../../../../app/submission/form/submission-form.component.scss'],
+  templateUrl: './submission-form.component.html',
+  //templateUrl: '../../../../../app/submission/form/submission-form.component.html'
+})
+
+export class SubmissionFormComponent  extends BaseComponent{
+
+}
