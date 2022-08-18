5b51b6fa7d5ab5716c08762864256798c7a6c939
Pragash <pragasra@gmail.com>
2022-08-18 09:37:00 -0400

    moving header.ts changes into ospr theme

--- src/themes/ospr/app/header/header.component.ts
+++ src/themes/ospr/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuService as BaseComponentMenuService } from '../../../../app/shared/menu/menu.service';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
 
 /**
@@ -12,4 +13,28 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
   //templateUrl: '../../../../app/header/header.component.html',
 })
 export class HeaderComponent extends BaseComponent {
+
+    // OSPR code start
+    public locationPath: string;
+    // OSPR code end
+
+    constructor(
+      private menuService2: BaseComponentMenuService
+    ) {
+       super(menuService2)
+      // OSPR code start
+        this.getPath();
+        // OSPR code end
+      
+    }
+
+        // OSPR code start
+        public getPath(): string {
+          this.locationPath = document.location.href;
+          if(this.locationPath.indexOf("#") > 0) {
+            this.locationPath.substring(this.locationPath.indexOf("#") + 1);
+          }
+          return this.locationPath;
+        }
+        // OSPR code end
 }
