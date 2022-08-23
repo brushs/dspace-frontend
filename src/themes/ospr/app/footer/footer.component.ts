a72ff9765c5d4ca81967f5e962fa457c848d09d0
Mohammad Raza <mraza@heartlandsoftware.ca>
2022-08-23 10:44:34 -0400

    fixed footer bugs

--- src/themes/ospr/app/footer/footer.component.ts
+++ src/themes/ospr/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
+import { KlaroService as BaseComponentKlaroService } from '../../../../app/shared/cookies/klaro.service';
 
 @Component({
   selector: 'ds-footer',
@@ -9,4 +10,18 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
   //templateUrl: '../../../../app/footer/footer.component.html'
 })
 export class FooterComponent extends BaseComponent {
+  public locationPath: string;
+
+  constructor(@Optional() private cookiesKlaro: BaseComponentKlaroService) {
+    super(cookiesKlaro);
+    this.getPath();
+  }
+
+  public getPath(): string {
+    this.locationPath = document.location.href;
+    if(this.locationPath.indexOf("#") > 0) {
+      return this.locationPath.substring(0, this.locationPath.indexOf("#"));
+    }
+    return this.locationPath;
+  }
 }
