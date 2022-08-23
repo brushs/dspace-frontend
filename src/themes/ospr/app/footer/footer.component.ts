2c69cb01d557ff54cc30e0bd7adcc6d9c8678c26
Mohammad Raza <mraza@heartlandsoftware.ca>
2022-08-23 10:31:43 -0400

    Revert "fixed top of page link"
    This reverts commit 20fdca3106fe5cbfdde0d06e66a42283d127da2e.

--- src/themes/ospr/app/footer/footer.component.ts
+++ src/themes/ospr/app/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuService as BaseComponentMenuService} from '../shared/menu/menu.service';
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
 
 @Component({
@@ -10,20 +9,4 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
   //templateUrl: '../../../../app/footer/footer.component.html'
 })
 export class FooterComponent extends BaseComponent {
-  public locationPath: string;
-
-  constructor(
-    private menuService: BaseComponentMenuService
-  ) {
-      super(menuService);
-      this.getPath();
-  }
-
-  public getPath(): string {
-    this.locationPath = document.location.href;
-    if(this.locationPath.indexOf("#") > 0) {
-      return this.locationPath.substring(0, this.locationPath.indexOf("#"));
-    }
-    return this.locationPath;
-  }
 }
